b99644d5408eee3b3cb0fa03cefd14159ca3a38c
Luis Matute <luismatute@ymail.com>
2018-10-25 14:49:01 -0400

    added automated rmakdown generation

---
+++ purrr for processing.R
@@ -0,0 +1,23 @@
+render_report <-function(regionvar){
+  template <-"steakArticleParams.Rmd"
+  outfile <-sprintf("steakArticle_%s.html",regionvar)
+  parameters <-list(region = regionvar)
+  rmarkdown::render(template,
+                    output_file=outfile,
+                    params=parameters)
+  invisible(TRUE)
+}
+
+render_report("Pacific")
+
+
+#R code to use the custom function with purrr
+
+
+library(purrr)
+params_list <- list(list("East North Central",
+                         "East South Central", "Middle Atlantic",
+                         "Mountain", "New England", "Pacific",
+                         "South Atlantic", "West North Central",
+                         "West South Central"))
+purrr::pmap(params_list,render_report)
